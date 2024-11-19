497bb1076fe0d4fdc9a929dea7beb4ee7e3478e7
nekobean <nobepon+github@gmail.com>
2024-11-19 20:28:27 +0900

    [add] 期待値計算クラスを追加

--- src/mahjong/core/expected_score_calculator.cpp
+++ src/mahjong/core/expected_score_calculator.cpp
@@ -1,24 +1,28 @@
 #include "expected_score_calculator.hpp"
 
-#include <boost/range/iterator_range_core.hpp>
-
-#include <numeric>
 #undef NDEBUG
 #include <cassert>
+#include <numeric>
+
+#include <boost/graph/graph_utility.hpp>
 
 #include "mahjong/core/necessary_tile_calculator.hpp"
+#include "mahjong/core/score_calculator.hpp"
 #include "mahjong/core/shanten_calculator.hpp"
+#include "mahjong/core/string.hpp"
 #include "mahjong/core/unnecessary_tile_calculator.hpp"
+
+using namespace player_impl;
+constexpr int L = 64;
 
 namespace mahjong
 {
-
-int64_t discard_all(const Hand &hand)
+int64_t calc_disc2(const HandType &hand)
 {
     int64_t ret = 0LL;
 
-    for (int i = 0; i < 34; ++i) {
-        if (hand.counts[i] > 0) {
+    for (int i = 0; i < K; ++i) {
+        if (hand[i] > 0) {
             ret |= 1LL << i;
         }
     }
@@ -26,12 +30,12 @@ int64_t discard_all(const Hand &hand)
     return ret;
 }
 
-int64_t wait_all(const Hand &hand)
+int64_t calc_wait2(const HandType &hand)
 {
     int64_t ret = 0LL;
 
-    for (int i = 0; i < 34; ++i) {
-        if (hand.counts[i] < 4) {
+    for (int i = 0; i < K; ++i) {
+        if (hand[i] < 4) {
             ret |= 1LL << i;
         }
     }
@@ -39,81 +43,274 @@ int64_t wait_all(const Hand &hand)
     return ret;
 }
 
-int distance(const Hand &hand, const Hand &origin)
+int distance(const std::vector<int> &hand, const std::vector<int> &origin)
 {
     return std::inner_product(
-        hand.counts.begin(), hand.counts.begin() + 34, origin.counts.begin(), 0,
-        std::plus<int>(), [](const int x, const int y) { return std::max(x - y, 0); });
+        hand.begin(), hand.end(), origin.begin(), 0, std::plus<int>(),
+        [](const int x, const int y) { return std::max(x - y, 0); });
 }
 
-ExpectedScoreCalculator::Vertex
-ExpectedScoreCalculator::draw(Graph &graph, Desc &desc1, Desc &desc2, Hand &hand,
-                              const int num, const Hand &origin, const int sht_org,
-                              const Params &params)
+std::vector<int> encode(const std::vector<int> &hand, const std::vector<int> &reds)
 {
-    CacheKey key(hand);
-    if (const auto itr = desc1.find(key); itr != desc1.end()) {
+    std::vector<int> ret(2 * K);
+
+    for (int i = 0; i < K; ++i) {
+        ret[i] = hand[i] - reds[i];
+        ret[K + i] = reds[i];
+    }
+
+    return ret;
+}
+
+HandType create_wall(const Round &round, const MyPlayer &player)
+{
+    HandType wall{0};
+    std::fill(wall.begin(), wall.begin() + K, 4);
+    wall[Tile::RedManzu5] = 1;
+    wall[Tile::RedPinzu5] = 1;
+    wall[Tile::RedSouzu5] = 1;
+
+    for (auto tile : round.dora_indicators) {
+        if (tile == Tile::RedManzu5) {
+            wall[Tile::Manzu5]--;
+            wall[Tile::RedManzu5]--;
+        }
+        else if (tile == Tile::RedPinzu5) {
+            wall[Tile::Pinzu5]--;
+            wall[Tile::RedPinzu5]--;
+        }
+        else if (tile == Tile::RedSouzu5) {
+            wall[Tile::Souzu5]--;
+            wall[Tile::RedSouzu5]--;
+        }
+        else {
+            wall[tile]--;
+        }
+    }
+
+    for (int i = 0; i < K; ++i) {
+        if (i == Tile::Manzu5) {
+            wall[i] -= player.hand[Tile::Manzu5];
+            wall[Tile::RedManzu5] -= player.hand[Tile::RedManzu5];
+        }
+        else if (i == Tile::Pinzu5) {
+            wall[i] -= player.hand[Tile::Pinzu5];
+            wall[Tile::RedPinzu5] -= player.hand[Tile::RedPinzu5];
+        }
+        else if (i == Tile::Souzu5) {
+            wall[i] -= player.hand[Tile::Souzu5];
+            wall[Tile::RedSouzu5] -= player.hand[Tile::RedSouzu5];
+        }
+        else {
+            wall[i] -= player.hand[i];
+        }
+    }
+
+    for (const auto &meld : player.melds) {
+        for (auto tile : meld.tiles) {
+            if (tile == Tile::RedManzu5) {
+                wall[tile] -= 1;
+                wall[Tile::Manzu5] -= 1;
+            }
+            else if (tile == Tile::RedPinzu5) {
+                wall[tile] -= 1;
+                wall[Tile::Pinzu5] -= 1;
+            }
+            else if (tile == Tile::RedSouzu5) {
+                wall[tile] -= 1;
+                wall[Tile::Souzu5] -= 1;
+            }
+            else {
+                wall[tile] -= 1;
+            }
+        }
+    }
+
+    return wall;
+}
+
+std::vector<int> encode(const HandType &counts)
+{
+    std::vector<int> ret(2 * K, 0);
+
+    for (int i = 0; i < K; ++i) {
+        if (i == Tile::Manzu5) {
+            ret[i] = counts[Tile::Manzu5] - counts[Tile::RedManzu5];
+            ret[K + i] = counts[Tile::RedManzu5];
+        }
+        else if (i == Tile::Pinzu5) {
+            ret[i] = counts[Tile::Pinzu5] - counts[Tile::RedPinzu5];
+            ret[K + i] = counts[Tile::RedPinzu5];
+        }
+        else if (i == Tile::Souzu5) {
+            ret[i] = counts[Tile::Souzu5] - counts[Tile::RedSouzu5];
+            ret[K + i] = counts[Tile::RedSouzu5];
+        }
+        else {
+            ret[i] = counts[i];
+            ret[K + i] = 0;
+        }
+    }
+
+    return ret;
+}
+
+void draw(std::vector<int> &hand_reds, std::vector<int> &wall_reds, MyPlayer &player,
+          const int tile)
+{
+    ++hand_reds[tile]; // 手札に追加
+    --wall_reds[tile]; // 山から削除
+
+    // 手牌更新
+    player.hand[tile % K]++; // 手牌から削除
+    if (tile == 34 + Tile::Manzu5) {
+        player.hand[Tile::RedManzu5]++;
+    }
+    else if (tile == 34 + Tile::Pinzu5) {
+        player.hand[Tile::RedPinzu5]++;
+    }
+    else if (tile == 34 + Tile::Souzu5) {
+        player.hand[Tile::RedSouzu5]++;
+    }
+}
+
+void discard(std::vector<int> &hand_reds, std::vector<int> &wall_reds, MyPlayer &player,
+             const int tile)
+{
+    --hand_reds[tile]; // 手札から削除
+    ++wall_reds[tile]; // 山に追加
+
+    // 手牌更新
+    player.hand[tile % K]--; // 手牌から削除
+    if (tile == 34 + Tile::Manzu5) {
+        player.hand[Tile::RedManzu5]--;
+    }
+    else if (tile == 34 + Tile::Pinzu5) {
+        player.hand[Tile::RedPinzu5]--;
+    }
+    else if (tile == 34 + Tile::Souzu5) {
+        player.hand[Tile::RedSouzu5]--;
+    }
+}
+
+int ExpectedScoreCalculator::calc_score(MyPlayer &player, const int mode,
+                                        const int tile, const Params &params) const
+{
+    int win_tile2 = tile;
+    if (tile == 34 + Tile::Manzu5) {
+        win_tile2 = Tile::RedManzu5;
+    }
+    else if (tile == 34 + Tile::Pinzu5) {
+        win_tile2 = Tile::RedPinzu5;
+    }
+    else if (tile == 34 + Tile::Souzu5) {
+        win_tile2 = Tile::RedSouzu5;
+    }
+
+    Result result = ScoreCalculator::calc(round_, player, win_tile2,
+                                          WinFlag::Riichi | WinFlag::Tsumo);
+    if (!result.success) {
+        std::cout << to_mpsz(player.hand) << std::endl;
+        std::cout << result.err_msg << std::endl;
+    }
+    assert(result.success);
+
+    return result.score[0];
+}
+
+ExpectedScoreCalculator::Vertex ExpectedScoreCalculator::select1(
+    Graph &graph, Desc &cache1, Desc &cache2, std::vector<int> &hand_reds,
+    std::vector<int> &wall_reds, MyPlayer &player, const std::vector<int> &origin,
+    const int sht_org, const Params &params) const
+{
+    // キャッシュに存在する場合、その頂点を返す。
+    CacheKey key(hand_reds);
+    if (const auto itr = cache1.find(key); itr != cache1.end()) {
         return itr->second;
     }
 
-    auto [type, shanten, wait] = NecessaryTileCalculator::calc(hand, params.mode);
-    bool allow_tegawari = distance(hand, origin) + shanten < sht_org + params.extra;
-    wait = allow_tegawari ? wait_all(hand) : wait;
-
+    // 頂点を作成する。
     const Vertex vertex =
-        boost::add_vertex(std::valarray<double>(0., params.t_max + 1), graph);
-    desc1[key] = vertex;
+        boost::add_vertex(std::vector<double>((params.t_max + 1) * 3, 0), graph);
+    cache1[key] = vertex;
 
-    for (int i = 0; i < 34; ++i) {
-        if (wait & (1LL << i)) {
-            // 自摸前の残り枚数
-            const int weight = 4 - hand.counts[i];
+    // 有効牌を計算する。
 
-            // 状態遷移させる。
-            ++hand.counts[i];
-            const auto target =
-                discard(graph, desc1, desc2, hand, num + 1, origin, sht_org, params);
-            --hand.counts[i];
+    auto [type, shanten, wait] =
+        NecessaryTileCalculator::calc(player.hand, 0, params.mode);
+    bool allow_tegawari =
+        distance(hand_reds, origin) + shanten < sht_org + params.extra;
+    int64_t all = allow_tegawari ? calc_wait2(player.hand) : wait;
+    all |= all << K;
 
-            // エッジを追加する。
-            // 自摸前の手牌 -自摸枚数-> 自摸後の手牌
+    for (int i = 0; i < L; ++i) {
+        if (wall_reds[i] && (all & (1LL << i))) {
+            const int weight = wall_reds[i];
+
+            draw(hand_reds, wall_reds, player, i);
+
+            const auto target = select2(graph, cache1, cache2, hand_reds, wall_reds,
+                                        player, origin, sht_org, params);
+
             if (!boost::edge(vertex, target, graph).second) {
-                boost::add_edge(vertex, target, weight, graph);
+                const int score = (shanten == 0 && (wait & (1LL << i % K))
+                                       ? calc_score(player, type, i % K, params)
+                                       : 0);
+
+                boost::add_edge(vertex, target, {weight, score}, graph);
             }
+
+            discard(hand_reds, wall_reds, player, i);
         }
     }
 
     return vertex;
 }
 
-ExpectedScoreCalculator::Vertex
-ExpectedScoreCalculator::discard(Graph &graph, Desc &desc1, Desc &desc2, Hand &hand,
-                                 const int num, const Hand &origin, const int sht_org,
-                                 const Params &params)
+ExpectedScoreCalculator::Vertex ExpectedScoreCalculator::select2(
+    Graph &graph, Desc &cache1, Desc &cache2, std::vector<int> &hand_reds,
+    std::vector<int> &wall_reds, MyPlayer &player, const std::vector<int> &origin,
+    const int sht_org, const Params &params) const
 {
-    CacheKey key(hand);
-    if (const auto itr = desc2.find(key); itr != desc2.end()) {
+    // キャッシュに存在する場合、その頂点を返す。
+    CacheKey key(hand_reds);
+    if (const auto itr = cache2.find(key); itr != cache2.end()) {
         return itr->second;
     }
 
-    auto [type, shanten, disc] = UnnecessaryTileCalculator::calc(hand, params.mode);
-    bool allow_shanten_down = distance(hand, origin) + shanten < sht_org + params.extra;
-    disc = allow_shanten_down ? discard_all(hand) : disc;
+    // 有効牌を計算する。
+    auto [type, shanten, disc] =
+        UnnecessaryTileCalculator::calc(player.hand, 0, params.mode);
 
-    const Vertex vertex = boost::add_vertex(
-        std::valarray<double>(shanten == -1, params.t_max + 1), graph);
-    desc2[key] = vertex;
+    bool allow_shanten_down =
+        distance(hand_reds, origin) + shanten < sht_org + params.extra;
+    int64_t all = allow_shanten_down ? calc_disc2(player.hand) : disc;
+    all |= all << K;
 
-    for (int i = 0; i < 34; ++i) {
-        if (disc & (1LL << i)) {
-            --hand.counts[i];
-            const int weight = 4 - hand.counts[i];
-            const auto source =
-                draw(graph, desc1, desc2, hand, num - 1, origin, sht_org, params);
-            ++hand.counts[i];
+    // 頂点を作成する。
+    std::vector<double> vertex_data((params.t_max + 1) * 3, 0);
+    std::fill(vertex_data.begin(), vertex_data.begin() + (params.t_max + 1),
+              shanten <= 0);
+    std::fill(vertex_data.begin() + (params.t_max + 1),
+              vertex_data.begin() + (params.t_max + 1) * 2, shanten == -1);
+    const Vertex vertex = boost::add_vertex(vertex_data, graph);
+    cache2[key] = vertex;
+
+    for (int i = 0; i < L; ++i) {
+        if (hand_reds[i] && (all & (1LL << i))) {
+            discard(hand_reds, wall_reds, player, i);
+
+            const int weight = wall_reds[i];
+            const auto source = select1(graph, cache1, cache2, hand_reds, wall_reds,
+                                        player, origin, sht_org, params);
+
+            draw(hand_reds, wall_reds, player, i);
 
             if (!boost::edge(source, vertex, graph).second) {
-                boost::add_edge(source, vertex, weight, graph);
+                const int score =
+                    (shanten == -1 ? calc_score(player, type, i % K, params) : 0);
+
+                boost::add_edge(source, vertex, {weight, score}, graph);
             }
         }
     }
@@ -121,65 +318,103 @@ ExpectedScoreCalculator::discard(Graph &graph, Desc &desc1, Desc &desc2, Hand &h
     return vertex;
 }
 
-void ExpectedScoreCalculator::update(Graph &graph, const Desc &desc1, const Desc &desc2,
-                                     const Params &params)
+void ExpectedScoreCalculator::update(Graph &graph, const Desc &cache1,
+                                     const Desc &cache2, const Params &params) const
 {
     for (int t = params.t_max - 1; t >= params.t_min; --t) {
-        for (auto &[hand, vertex] : desc1) {
-            std::valarray<double> &prob = graph[vertex];
+        for (auto &[_, vertex] : cache1) {
+            VertexData &value = graph[vertex];
+            double *tenpai_prob = value.data();
+            double *win_prob = value.data() + (params.t_max + 1);
+            double *exp_value = value.data() + (params.t_max + 1) * 2;
 
-            for (const auto &edge :
-                 boost::make_iterator_range(boost::out_edges(vertex, graph))) {
-                const Vertex target = boost::target(edge, graph);
+            for (auto [first, last] = boost::out_edges(vertex, graph); first != last;
+                 ++first) {
+                const auto target = boost::target(*first, graph);
+                const auto [weight, score] = graph[*first];
+                VertexData &value2 = graph[target];
+                double *tenpai_prob2 = value2.data();
+                double *win_prob2 = value2.data() + (params.t_max + 1);
+                double *exp_value2 = value2.data() + (params.t_max + 1) * 2;
 
-                prob[t] += graph[edge] * (graph[target][t + 1] - prob[t + 1]);
+                tenpai_prob[t] += weight * (tenpai_prob2[t + 1] - tenpai_prob[t + 1]);
+                win_prob[t] += weight * (win_prob2[t + 1] - win_prob[t + 1]);
+                exp_value[t] +=
+                    weight * (std::max(static_cast<double>(score), exp_value2[t + 1]) -
+                              exp_value[t + 1]);
             }
 
-            prob[t] = prob[t + 1] + prob[t] / (params.sum - t);
+            // tenpai_prob[t] = tenpai_prob[t + 1] + tenpai_prob[t] / (params.sum - t);
+            // win_prob[t] = win_prob[t + 1] + win_prob[t] / (params.sum - t);
+            // exp_value[t] = exp_value[t + 1] + exp_value[t] / (params.sum - t);
+            tenpai_prob[t] = tenpai_prob[t + 1] + tenpai_prob[t] / params.sum;
+            win_prob[t] = win_prob[t + 1] + win_prob[t] / params.sum;
+            exp_value[t] = exp_value[t + 1] + exp_value[t] / params.sum;
         }
 
-        for (auto &[hand, vertex] : desc2) {
-            std::valarray<double> &prob = graph[vertex];
+        for (auto &[_, vertex] : cache2) {
+            VertexData &value = graph[vertex];
+            double *tenpai_prob = value.data();
+            double *win_prob = value.data() + (params.t_max + 1);
+            double *exp_value = value.data() + (params.t_max + 1) * 2;
 
-            for (const auto &edge :
-                 boost::make_iterator_range(boost::in_edges(vertex, graph))) {
-                const Vertex source = boost::source(edge, graph);
+            for (auto [first, last] = boost::in_edges(vertex, graph); first != last;
+                 ++first) {
+                const auto source = boost::source(*first, graph);
+                VertexData &value2 = graph[source];
+                double *tenpai_prob2 = value2.data();
+                double *win_prob2 = value2.data() + (params.t_max + 1);
+                double *exp_value2 = value2.data() + (params.t_max + 1) * 2;
 
-                prob[t] = std::max(prob[t], graph[source][t]);
+                tenpai_prob[t] = std::max(tenpai_prob[t], tenpai_prob2[t]);
+                win_prob[t] = std::max(win_prob[t], win_prob2[t]);
+                exp_value[t] = std::max(exp_value[t], exp_value2[t]);
             }
         }
     }
 }
 
-std::tuple<std::vector<Stat>, std::size_t>
-ExpectedScoreCalculator::operator()(Hand &hand, const Params &params)
+std::tuple<std::vector<ExpectedScoreCalculator::Stat>, std::size_t>
+ExpectedScoreCalculator::calc(const Round &round, MyPlayer &player,
+                              const Params &params)
 {
-    const int num = hand.num_tiles();
+    round_ = round;
 
     assert(params.t_min >= 0);
     assert(params.t_min < params.t_max);
-    assert(num % 3 == 2);
+    assert(player.num_tiles() % 3 == 2);
 
-    const auto [type, shanten] = ShantenCalculator::calc(hand, params.mode);
+    auto [type, shanten] = ShantenCalculator::calc(player.hand, 0, params.mode);
 
     Graph graph;
-    Desc desc1, desc2;
-    Hand origin_hand = hand;
+    Desc cache1, cache2;
 
-    // Create a transition graph.
-    discard(graph, desc1, desc2, hand, num, origin_hand, shanten, params);
+    auto hand_reds = encode(player.hand);
+    auto wall2 = create_wall(round, player);
+    auto wall_reds = encode(wall2);
 
-    // Calculate probabilities and expected values.
-    update(graph, desc1, desc2, params);
+    select2(graph, cache1, cache2, hand_reds, wall_reds, player,
+            std::vector<int>{hand_reds}, shanten, params);
+    update(graph, cache1, cache2, params);
 
+    // 結果を取得する。
     std::vector<Stat> stats;
-    for (int i = 0; i < 34; ++i) {
-        if (hand.counts[i] > 0) {
-            --hand.counts[i];
-            if (const auto itr = desc1.find(hand); itr != desc1.end()) {
-                stats.emplace_back(Stat{i, graph[itr->second]});
+    for (int i = 0; i < L; ++i) {
+        if (hand_reds[i] > 0) {
+            --hand_reds[i];
+            if (const auto itr = cache1.find(hand_reds); itr != cache1.end()) {
+                const VertexData &value = graph[itr->second];
+                std::vector<double> tenpai_prob(value.begin(),
+                                                value.begin() + (params.t_max + 1));
+                std::vector<double> win_prob(value.begin() + (params.t_max + 1),
+                                             value.begin() + (params.t_max + 1) * 2);
+                std::vector<double> exp_value(value.begin() + (params.t_max + 1) * 2,
+                                              value.end());
+
+                stats.emplace_back(
+                    Stat{i % K, i > K, tenpai_prob, win_prob, exp_value});
             }
-            ++hand.counts[i];
+            ++hand_reds[i];
         }
     }
 
@@ -187,5 +422,4 @@ ExpectedScoreCalculator::operator()(Hand &hand, const Params &params)
 
     return {stats, searched};
 }
-
 } // namespace mahjong
--- src/mahjong/core/expected_score_calculator.hpp
+++ src/mahjong/core/expected_score_calculator.hpp
@@ -1,40 +1,63 @@
 #ifndef MAHJONG_CPP_EXPECTED_SCORE_CALCULATOR
 #define MAHJONG_CPP_EXPECTED_SCORE_CALCULATOR
 
-#include <array>
 #include <map>
 #include <tuple>
-#include <valarray>
 #include <vector>
 
-#include <boost/container_hash/hash.hpp>
+#include "compare/player_impl.hpp"
+#include "compare/utils.hpp"
+
 #include <boost/graph/adjacency_list.hpp>
 
-#include "compare/utils.hpp"
 #include "mahjong/types/types.hpp"
 
 namespace mahjong
 {
-/**
- * @brief Expected score calculator
- */
 class ExpectedScoreCalculator
 {
+  public:
+    struct Params
+    {
+        int t_min = 0; // 計算対象の巡目の最小値
+        int t_max = 0; // 計算対象の巡目の最大値
+        int sum = 0;   // 山の枚数
+        int extra = 0; // 現在の手牌からこの回数分は牌を交換できるものとする
+        int mode = 7;                     // 計算対象の向聴数の種類
+        bool enable_uradora = true;       // 裏ドラを考慮するか
+        bool enable_reddora = true;       // 赤ドラを考慮するか
+        bool enable_shanten_down = true;  // 向聴落としを考慮するか
+        bool enable_tegawari = true;      // 手替わりを考慮するか
+        bool enable_double_riichi = true; // ダブル立直を考慮するか
+        bool enable_ippatsu = true;       // 一発を考慮するか
+        bool enable_under_the_sea = true; // 海底撈月を考慮するか
+    };
+
+    struct Stat
+    {
+        const int tile;                        // 打牌
+        const bool is_red;                     // 赤ドラか
+        const std::vector<double> tenpai_prob; // 和了確率
+        const std::vector<double> win_prob;    // 和了確率
+        const std::vector<double> exp_value;   // 和了確率
+    };
+
+  private:
     struct CacheKey
     {
-        CacheKey(const Hand &hand) : manzu(0), pinzu(0), souzu(0), honors(0)
+        CacheKey(const std::vector<int> &hand) : manzu(0), pinzu(0), souzu(0), honors(0)
         {
-            manzu = std::accumulate(hand.counts.begin(), hand.counts.begin() + 9, 0,
+            manzu = std::accumulate(hand.begin(), hand.begin() + 9, 0,
                                     [](int x, int y) { return x * 8 + y; });
-            pinzu = std::accumulate(hand.counts.begin() + 9, hand.counts.begin() + 18,
-                                    0, [](int x, int y) { return x * 8 + y; });
-            souzu = std::accumulate(hand.counts.begin() + 18, hand.counts.begin() + 27,
-                                    0, [](int x, int y) { return x * 8 + y; });
-            honors = std::accumulate(hand.counts.begin() + 27, hand.counts.begin() + 34,
-                                     0, [](int x, int y) { return x * 8 + y; });
-            honors |= hand.counts[Tile::RedManzu5] << 21;
-            honors |= hand.counts[Tile::RedPinzu5] << 22;
-            honors |= hand.counts[Tile::RedSouzu5] << 23;
+            pinzu = std::accumulate(hand.begin() + 9, hand.begin() + 18, 0,
+                                    [](int x, int y) { return x * 8 + y; });
+            souzu = std::accumulate(hand.begin() + 18, hand.begin() + 27, 0,
+                                    [](int x, int y) { return x * 8 + y; });
+            honors = std::accumulate(hand.begin() + 27, hand.begin() + 34, 0,
+                                     [](int x, int y) { return x * 8 + y; });
+            honors |= hand[Tile::Manzu5 + 34] << 21;
+            honors |= hand[Tile::Pinzu5 + 34] << 22;
+            honors |= hand[Tile::Souzu5 + 34] << 23;
         }
 
         bool operator<(const CacheKey &other) const
@@ -49,25 +72,35 @@ class ExpectedScoreCalculator
         int32_t honors;
     };
 
-    using Graph =
-        boost::adjacency_list<boost::listS, boost::vecS, boost::bidirectionalS,
-                              std::valarray<double>, int>;
+    using VertexData = std::vector<double>;
+    using EdgeData = std::pair<int, int>;
+    using Graph = boost::adjacency_list<boost::listS, boost::vecS,
+                                        boost::bidirectionalS, VertexData, EdgeData>;
     using Vertex = Graph::vertex_descriptor;
     using Edge = Graph::edge_descriptor;
     using Desc = std::map<CacheKey, Vertex>;
 
-  private:
-    Vertex draw(Graph &graph, Desc &desc1, Desc &desc2, Hand &hand, int num,
-                const Hand &origin, int sht_org, const Params &params);
-    Vertex discard(Graph &graph, Desc &desc1, Desc &desc2, Hand &hand, int num,
-                   const Hand &origin, int sht_org, const Params &params);
-    void update(Graph &graph, const Desc &desc1, const Desc &desc2,
-                const Params &params);
-
   public:
-    std::tuple<std::vector<Stat>, std::size_t> operator()(Hand &hand,
-                                                          const Params &params);
+    std::tuple<std::vector<Stat>, std::size_t>
+    calc(const Round &round, MyPlayer &player, const Params &params);
+
+  private:
+    int calc_score(MyPlayer &player, const int mode, const int tile,
+                   const Params &params) const;
+    Vertex select1(Graph &graph, Desc &cache1, Desc &cache2,
+                   std::vector<int> &hand_reds, std::vector<int> &wall_reds,
+                   MyPlayer &player, const std::vector<int> &origin_reds, int sht_org,
+                   const Params &params) const;
+    Vertex select2(Graph &graph, Desc &cache1, Desc &cache2,
+                   std::vector<int> &hand_reds, std::vector<int> &wall_reds,
+                   MyPlayer &player, const std::vector<int> &origin_reds, int sht_org,
+                   const Params &params) const;
+    void update(Graph &graph, const Desc &cache1, const Desc &cache2,
+                const Params &params) const;
+
+  private:
+    Round round_;
 };
 } // namespace mahjong
 
-#endif /* MAHJONG_CPP_EXPECTED_SCORE_CALCULATOR */
+#endif
